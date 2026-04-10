ac982a888069be8f06827838c9aaed1eb6334b31
Mohammed Shariq <108349504+shariksait@users.noreply.github.com>
2026-04-10 17:32:33 +0100

    Added Auto PR review, Java and Gradle (#2)

---
+++ src/main/java/com/example/api/client/ExternalApiClient.java
@@ -0,0 +1,15 @@
+package com.example.api.client;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ExternalApiClient {
+
+  private final RestTemplate restTemplate = new RestTemplate();
+
+  public String fetchData() {
+    String url = "https://api.agify.io?name=john";
+    return restTemplate.getForObject(url, String.class);
+  }
+}
--- src/main/java/com/example/api/controller/UserController.java
+++ src/main/java/com/example/api/controller/UserController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private final UserService userService;
+  private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
 
-    @PostMapping
-    public UserResponse createUser(@RequestBody UserRequest request) {
-        return userService.processUser(request);
-    }
+  @PostMapping
+  public UserResponse createUser(@RequestBody UserRequest request) {
+    return userService.processUser(request);
+  }
 }
